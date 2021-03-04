d5a64dcd6c09be2c2c550ea97b1afe7392aab19f
timcguire <tom_i_mcguire@yahoo.com>
2021-03-03 21:52:58 -0500

    cleaned up the infix.cpp
    MUCH CLEANER stack function

--- Module7/Module7/infix.cpp
+++ Module7/Module7/infix.cpp
@@ -6,18 +6,10 @@
 using namespace std;
 
 
-bool check_operator(string oper)
-{
-	if (oper == "+" || oper == "-" || oper == "*" || oper == "/")
-	{
-		return true;
-	}
-	return false;
-}
 bool read_operator(string oper, stack & S, bool error)
 {
 	string complete;
-	if (oper == "+")
+	if (oper == "+" || oper == "-" || oper == "*" || oper == "/")
 	{
 		if (S.empty() == true)
 		{
@@ -38,85 +30,8 @@ bool read_operator(string oper, stack & S, bool error)
 		string left = S.top();
 		S.pop();
 		complete = "(" + left + oper + right + ")";
-		S.push(complete);
+		S.push(complete);	
 		
-		
-		
-	}
-
-	if (oper == "-")
-	{
-		if (S.empty() == true)
-		{
-			cout << "too many operators not enough operands\n";
-			error = true;
-			return error;
-		}
-
-		string right = S.top();
-		S.pop();
-
-		if (S.empty() == true)
-		{
-			cout << "too many operators not enough operands\n";
-			error = true;
-			return error;
-		}
-
-		string left = S.top();
-		S.pop();
-		complete = "(" + left + oper + right + ")";
-		S.push(complete);
-	}
-
-	if (oper == "/")
-	{
-		if (S.empty() == true)
-		{
-			cout << "too many operators not enough operands\n";
-			error = true;
-			return error;
-		}
-
-		string right = S.top();
-		S.pop();
-
-		if (S.empty() == true)
-		{
-			cout << "too many operators not enough operands\n";
-			error = true;
-			return error;
-		}
-
-		string left = S.top();
-		S.pop();
-		string complete = "(" + left + oper + right + ")";
-		S.push(complete);
-	}
-
-	if (oper == "*")
-	{
-		if (S.empty() == true)
-		{
-			cout << "too many operators not enough operands\n";
-			error = true;
-			return error;
-		}
-
-		string right = S.top();
-		S.pop();
-
-		if (S.empty() == true)
-		{
-			cout << "too many operators not enough operands\n";
-			error = true;
-			return error;
-		}
-
-		string left = S.top();
-		S.pop();
-		complete = "(" + left + oper + right + ")";
-		S.push(complete);
 	}
 
 	
@@ -128,7 +43,7 @@ Course: Data Structures and algorithm Analysis (COP3530) Professor: Dr. Lofton B
 Due Date: March 3, 2021  Due Time: 11:59 PM
 Total Points: 25 Assignment 7: Stack program
 Description: asks the user for a postfix expression as input, and it should output the corresponding fully
-parenthesized infex expression. The program should also ask the user if he/she would like to
+parenthesized infix expression. The program should also ask the user if he/she would like to
 do another conversion. If so, the user should be able to enter another postfix expression; otherwise
 the program should terminate. The stack is implemeneted using a singly-linked list. The driver
 infix.cpp should include the definition and declaration files for the class Stack, stack.cpp, stack.h respectively.
@@ -147,9 +62,6 @@ int main()
 		cout << "Input postfix: \n";
 		getline(cin, input);
 		int lowcounter = 0;
-		int left = 0;
-		int right = 0;
-		int size = 0;
 		bool escape = false;
 		bool error = false;
 
@@ -167,7 +79,7 @@ int main()
 				string character = "";
 				character += input[i];
 
-				if (check_operator(character) == true)
+				if (character == "+" || character == "-" || character == "*" || character == "/")
 				{
 					error = read_operator(character, *S, error);
 					lowcounter = 0;
@@ -189,14 +101,12 @@ int main()
 					{
 
 						S->push(character);
-						size++;
 
 						if (input.length() == 1)
 						{
 							string infix = "(" + S->top() + ")";
 							S->pop();
 							S->push(infix);
-							size = 1;
 						}
 					}
 
